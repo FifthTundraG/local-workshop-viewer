7b7aa1e10e736538bfa11f8bab1f6981af425041
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2024-06-12 09:58:37 -0400

    Add automatic Steam library location detection and `requirements.txt`

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # img-to-turtle
 Shows every addon installed locally on the users machine for a specified game.
 
-Requires the requests library, install it with
-`python3 -m pip install --upgrade requests`.
+Requires the requests, dotenv, and vdf libraries, install them with the following command:
+`pip install -r requirements.txt`
 
 ## Obtaining an API key
 To make requests to the Steam Web API for collecting workshop data, you will need a Steam Web API Key. To obtain one, visit https://steamcommunity.com/dev/apikey. You will need the Steam Mobile Authenticator enabled. When registering, it will prompt you for a domain name. If you do not own a domain, feel free to put anything in this field. Afterwards, you will receive a Base64 encoded string as your key. Put this key in the .env file under `STEAM_WEB_API_KEY=`.
--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@
 import platform # for finding the OS
 import os # for finding the IDs of the workshop addons & api key env variables
 import logging # For logging important information to the console
-import requests # for Steam API requests
 from datetime import datetime # For showing the date & time of the output log's info
+
+import requests # for Steam API requests
 from dotenv import load_dotenv # For securely storing API keys
+import vdf # for finding the library folders automagically
 
 defaultSteamDir: dict[str, str] = {
     "Windows": "C:\\Program Files (x86)\\Steam",
@@ -23,24 +25,33 @@ def main() -> None:
         logging.critical("No API key was provided in the \".env\" file. See README.md for more details.")
         return
 
-    # Get the user's steam directory
-    if (platform.system() not in defaultSteamDir.keys()):
-        logging.warn("Your operating system is not directly supported. The default steam directory listed below may be incorrect.")
-
-    steamDir: str = input(f"Steam directory [{defaultSteamDir[platform.system()]}]: ").strip() or defaultSteamDir[platform.system()]
+    # Use the libraryfolders.vdf file in "STEAMDIR/steamapps" to find all the library folders
+    if platform.system() == "Windows": #* since windows uses backslashes for directory paths, we need to differentiate between them and UNIX-like systems
+        libraryfolders = vdf.load(open(defaultSteamDir[platform.system()]+"\\steamapps\\libraryfolders.vdf"))
+    else:
+        libraryfolders = vdf.load(open(defaultSteamDir[platform.system()]+"/steamapps/libraryfolders.vdf"))
+    print("Type the identifier associtated with the Steam library folder your game is installed in:")
+    for i in enumerate(libraryfolders["libraryfolders"].keys()):
+        print(f"({i[1]}) {libraryfolders["libraryfolders"][i[1]]["path"]}")
+    librarySelection = input("Library ID: ")
+    if librarySelection not in libraryfolders["libraryfolders"].keys():
+        logging.critical("Invalid selection.")
+        return
+    steamDir = libraryfolders["libraryfolders"][librarySelection]["path"]
 
     # Get the user's game ID
     GAME_ID = input("Game ID: ") # todo: make this required
     if GAME_ID == None or GAME_ID == "":
         logging.critical("An input for Game ID is required to continue.")
         return
-    GAME_ID = int(GAME_ID) # i can't put this where the input is because we need to check if the input is blank and it will error out complaining that "" isn't a number when it's at definition
+    GAME_ID = int(GAME_ID) #* i can't put this where the input is because we need to check if the input is blank and it will error out complaining that "" isn't a number when it's at definition
 
     # Using the game ID, get the directory where workshop content is stored
-    if platform.system() == "Windows": # windows uses backslashes for dir paths so we need to differentiate between them and UNIX-like systems
+    if platform.system() == "Windows": #* since windows uses backslashes for directory paths, we need to differentiate between them and UNIX-like systems
         workshopDir = f"{steamDir}\\steamapps\\workshop\\content\\{GAME_ID}"
     else:
         workshopDir = f"{steamDir}/steamapps/workshop/content/{GAME_ID}"
+
     if os.path.exists(workshopDir) != True:
         logging.critical(f"Workshop directory for game {GAME_ID} does not exist. Directory: {workshopDir}")
         return
@@ -68,16 +79,12 @@ def main() -> None:
             return
     
     # Make a request to Steam using the game ID to get the name of the game for logging purposes
-    gameIDRequest = requests.get(f'https://store.steampowered.com/api/appdetails?appids={GAME_ID}') # For an example request for gameIDRequest, use this link for Portal: https://store.steampowered.com/api/appdetails?appids=400
-    gameIDRequest.raise_for_status() 
-    if gameIDRequest.json()[str(GAME_ID)]["success"] != True:
+    gameDetails = requests.get(f'https://store.steampowered.com/api/appdetails?appids={GAME_ID}') # For an example request for gameDetails, use this link for Portal: https://store.steampowered.com/api/appdetails?appids=400
+    gameDetails.raise_for_status() 
+    if gameDetails.json()[str(GAME_ID)]["success"] != True:
         logging.error("Request to Steam Web API for game id "+str(GAME_ID)+" was not successful.")
         return
-    GAME_NAME: str = gameIDRequest.json()[str(GAME_ID)]["data"]["name"]
-
-    # Write the name of the game and current date to output.txt
-    f = open("output.txt", "w", encoding="utf-8") #* we get the following error in certain situations without the encoding bit: UnicodeEncodeError: 'charmap' codec can't encode characters in position 29-36: character maps to <undefined>
-    f.write(f"{GAME_NAME} Local Workshop Addon List - {datetime.now()}\n\n")
+    GAME_NAME: str = gameDetails.json()[str(GAME_ID)]["data"]["name"]
 
     # Get all addon IDs from the workshop content dir and make a request to Steam for their details
     addonIDs: list[str] = next(os.walk(workshopDir))[1] # todo: check if this is none
@@ -95,8 +102,15 @@ def main() -> None:
     # if fileDetails.json()["status"]["code"] != 1: # if this field is present then an error occured, 1=OK, any other number is an error
     #     logging.error(f"Request to Steam Web API for workshop data failed. Steam error code {str(fileDetails.json()["status"]["code"])}.")
     #     return
+
+    # Handle writing file and printing output for addon details
+    f = open("output.txt", "w", encoding="utf-8") #* we get the following error in certain situations without the encoding bit: UnicodeEncodeError: 'charmap' codec can't encode characters in position 29-36: character maps to <undefined>
+    # Write & print title
+    msg = f"{GAME_NAME} Local Workshop Addon List - {datetime.now()}\n"
+    f.write(f"{msg}\n")
+    print(msg)
+
     addonsWithErrors: list[str] = []
-    print(f"{GAME_NAME} Local Workshop Addon List - {datetime.now()}\n")
     for i in fileDetails.json()["response"]["publishedfiledetails"]:
         if i["result"] != 1: # 1=OK, any other number is an error
             msg = f"Error while processing addon with ID {i["publishedfileid"]}. Steam error code {i["result"]}"
